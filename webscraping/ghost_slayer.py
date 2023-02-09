ac027b93117919108a65866034e5e5f91c85aec5
collinpead <collin.pead@knights.ucf.edu>
2023-02-09 18:31:57 -0500

    Modified to correct behavior for games with 0 streamers live.

--- webscraping/ghost_slayer.py
+++ webscraping/ghost_slayer.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from config import config
 # Python postgreSQL connector library.
 import psycopg2
+from datetime import datetime
 
 # Uses config parameters to establish a connection to the database.
 def connect():
@@ -25,7 +26,11 @@ def process_viewers(viewers):
 
     return int(num_viewers)
 
-def fetch_twitch():
+# Slay ghosts processes games flagged for low viewership for data integrity.
+# If 0 people are currently streaming a game, selenium will fetch the number of followers instead of the number of viewers.
+# This causes fradulent viewership in the twitch records table.
+# Slay ghosts corrects the fradulent viewership, while preserving runtime by only checking flagged games.
+def slay_ghosts():
     conn = None
     # Connect to the postgreSQL database.
     try:
@@ -38,7 +43,7 @@ def fetch_twitch():
             # Create a cursor with the database connection.
             csrs = conn.cursor()
             print("Successfully connected to database.")
-            csrs.execute("SELECT ss_name, sg_name FROM sg_address")
+            csrs.execute("SELECT ss_name, sg_name FROM sg_address WHERE low_views_flag = true")
             names = csrs.fetchall()
 
             opts = webdriver.FirefoxOptions()
@@ -57,18 +62,24 @@ def fetch_twitch():
                     driver.implicitly_wait(5)
                     driver.get("https://www.twitch.tv/directory/game/" + name)
                     try:
-                        viewers_xpath = "/html/body/div[1]/div/div[2]/div/main/div[1]/div[3]/div/div/div/div/div/section/div[1]/div[2]/div/div[2]/div[2]/div[1]/p"
-                        viewers = driver.find_element(By.XPATH, viewers_xpath).text
+                        ghost_town_xpath = "/html/body/div[1]/div/div[2]/div/main/div[1]/div[3]/div/div/div/div/div/section/div[4]/div/h3"
+                        driver.find_element(By.XPATH, ghost_town_xpath).text
                     except:
-                        print("Could not find viewers for " + name)
+                        try:
+                            viewers_xpath = "/html/body/div[1]/div/div[2]/div/main/div[1]/div[3]/div/div/div/div/div/section/div[1]/div[2]/div/div[2]/div[2]/div[1]/p"
+                            viewers = driver.find_element(By.XPATH, viewers_xpath).text
+                        except:
+                            print("Could not find viewers for " + name)
+                        else:
+                            num_viewers = process_viewers(viewers)
+                            csrs.execute("UPDATE twitch_records SET viewers = %s WHERE name = %s", (num_viewers, steam_name))
+                            conn.commit()
                     else:
-                        num_viewers = process_viewers(viewers)
-                        print(num_viewers)
-                        csrs.execute("INSERT INTO twitch_records (id, date, name, viewers) VALUES (default, CURRENT_DATE, %s, %s)", (steam_name, num_viewers))
+                        csrs.execute("UPDATE twitch_records SET viewers = %s WHERE name = %s", (0, steam_name))
                         conn.commit()
 
             driver.close()
             conn.close()
-            print("Twitch records session terminated.")
+            print("Ghosts have been slain.")
 
-fetch_twitch()
+slay_ghosts()
